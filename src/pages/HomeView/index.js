be4e4866faa4634f61e8d089c75b9bd04f38e7db
incarhu <jacksonhyl@163.com>
2019-08-02 23:56:30 +0800

    'fetch数据'

--- src/pages/HomeView/index.js
+++ src/pages/HomeView/index.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react'
 import './index.scss'
 
-const product = [
-    {"id": 34975, "url": require('./images/1.jpg'), "title": "进口香蕉1袋500g起「新人特惠」", "subTitle": "跟鲜花一起运过来的香蕉","price": 6.3, "oldPrice": 12.9},
-    {"id": 34785, "url": require('./images/2.jpg'), "title": "妃子笑荔枝500g「新人特惠」", "subTitle": "妃子笑嘻嘻，吃了好甜蜜","price": 9.9, "oldPrice": 26.9},
-    {"id": 23486, "url": require('./images/3.jpg'), "title": "千禧圣女果1盒500g「新人特惠」", "subTitle": "90斤女孩的饭后小甜点","price": 6.3, "oldPrice": 12.9},
-    {"id": 34975, "url": require('./images/1.jpg'), "title": "进口香蕉1袋500g起「新人特惠」", "subTitle": "跟鲜花一起运过来的香蕉","price": 6.3, "oldPrice": 12.9},
-    {"id": 34785, "url": require('./images/2.jpg'), "title": "妃子笑荔枝500g「新人特惠」", "subTitle": "妃子笑嘻嘻，吃了好甜蜜","price": 9.9, "oldPrice": 26.9},
-    {"id": 23486, "url": require('./images/3.jpg'), "title": "千禧圣女果1盒500g「新人特惠」", "subTitle": "90斤女孩的饭后小甜点","price": 6.3, "oldPrice": 12.9},
-]
+// const product = [
+//     {"id": 34975, "url": require('./images/1.jpg'), "title": "进口香蕉1袋500g起「新人特惠」", "subTitle": "跟鲜花一起运过来的香蕉","price": 6.3, "oldPrice": 12.9},
+//     {"id": 34785, "url": require('./images/2.jpg'), "title": "妃子笑荔枝500g「新人特惠」", "subTitle": "妃子笑嘻嘻，吃了好甜蜜","price": 9.9, "oldPrice": 26.9},
+//     {"id": 23486, "url": require('./images/3.jpg'), "title": "千禧圣女果1盒500g「新人特惠」", "subTitle": "90斤女孩的饭后小甜点","price": 6.3, "oldPrice": 12.9},
+//     {"id": 34975, "url": require('./images/1.jpg'), "title": "进口香蕉1袋500g起「新人特惠」", "subTitle": "跟鲜花一起运过来的香蕉","price": 6.3, "oldPrice": 12.9},
+//     {"id": 34785, "url": require('./images/2.jpg'), "title": "妃子笑荔枝500g「新人特惠」", "subTitle": "妃子笑嘻嘻，吃了好甜蜜","price": 9.9, "oldPrice": 26.9},
+//     {"id": 23486, "url": require('./images/3.jpg'), "title": "千禧圣女果1盒500g「新人特惠」", "subTitle": "90斤女孩的饭后小甜点","price": 6.3, "oldPrice": 12.9},
+// ]
 
 class HomeView extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = { data: [] };
         this.handleSearch = this.handleSearch.bind(this)
+    }
+
+    componentDidMount() {
+        fetch('/mock/mock.json').then(res => {
+            if(res.ok) {
+                res.json().then(data => {
+                    this.setState({
+                        data
+                    })
+                })
+            }
+        })
     }
 
     handleSearch () {
@@ -41,7 +53,7 @@ class HomeView extends Component {
 
                 <section className="main">
                     <div className="banner-img"></div>
-                    {product.map((item, i) => {
+                    {this.state.data.map((item, i) => {
                         return (
                             <div className="product-list-item-wrap" key={i} onClick={this.toDetail.bind(this, item.id)}>
                                 <div className="product-item-container">
