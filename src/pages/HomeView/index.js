de5ce355b3779d55ca099838770a51b6aa7090c7
huyalong <sp123456>
2019-08-03 10:45:16 +0800

    抽离封装listItem

--- src/pages/HomeView/index.js
+++ src/pages/HomeView/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import './index.scss'
 
+import ListItem from '../../Components/ListItem'
+
 // const product = [
 //     {"id": 34975, "url": require('./images/1.jpg'), "title": "进口香蕉1袋500g起「新人特惠」", "subTitle": "跟鲜花一起运过来的香蕉","price": 6.3, "oldPrice": 12.9},
 //     {"id": 34785, "url": require('./images/2.jpg'), "title": "妃子笑荔枝500g「新人特惠」", "subTitle": "妃子笑嘻嘻，吃了好甜蜜","price": 9.9, "oldPrice": 26.9},
@@ -15,14 +17,15 @@ class HomeView extends Component {
         super(props);
         this.state = { data: [] };
         this.handleSearch = this.handleSearch.bind(this)
+        this.handleDetail = this.handleDetail.bind(this)
     }
 
     componentDidMount() {
         fetch('/mock/mock.json').then(res => {
-            if(res.ok) {
+            if(res.status === 200) {
                 res.json().then(data => {
                     this.setState({
-                        data
+                        data: data
                     })
                 })
             }
@@ -32,10 +35,12 @@ class HomeView extends Component {
     handleSearch () {
         console.log('搜索')
     }
-    toDetail(data) {
-        // console.log(`/product/${data}`)
+
+    // 详情
+    handleDetail(data) {
         this.props.history.push(`/product/${data}`)
     }
+    
     render() {
         return (
             <div className="home-view">
@@ -53,41 +58,8 @@ class HomeView extends Component {
 
                 <section className="main">
                     <div className="banner-img"></div>
-                    {this.state.data.map((item, i) => {
-                        return (
-                            <div className="product-list-item-wrap" key={i} onClick={this.toDetail.bind(this, item.id)}>
-                                <div className="product-item-container">
-                                    <div className="product-item">
-                                        <div className="item-image">
-                                            <img className="x-img" src={item.url} alt={item.url} />
-                                        </div>
-                                        <div className="item-detail">
-                                            <div className="item-title">
-                                                <p className="sub-title">{item.title}</p>
-                                                <p className="mess-title">{item.subTitle}</p>
-                                            </div>
-                                            <div className="item-tag">
-                                                <div className="product-item-tag">新人专享</div>
-                                            </div>
-                                            <div className="item-bottom">
-                                                <div className="item-price">
-                                                    <span>￥</span>
-                                                    <span className="price">{item.price}</span>
-                                                    <span className="has-line-through">
-                                                        ￥{item.oldPrice}
-                                                    </span>
-                                                </div>
-                                                <div className="product-item-cart">
-                                                    <span className="car-icon"></span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        )                   
-                    })}
                     
+                    <ListItem handleDetail={this.handleDetail} datas={this.state.data}/>
                 </section>                
             </div>
         );
