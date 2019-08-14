4ca425d41cb21b3339247167332a3df931ba4833
huyalong <sp123456>
2019-08-14 17:14:18 +0800

    修改

--- src/pages/ProductDetail/index.js
+++ src/pages/ProductDetail/index.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
 
 class ProductDetail extends Component {
-    constructor(props, context) {
-        super(props, context)
-        this.setState = {}
-    }
     render() {
         return (
             <div>详情页面</div>
