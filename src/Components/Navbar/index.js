1835e1f3ac1c20b9bc400e8e36efca5df76725ec
huyalong <sp123456>
2019-08-14 16:50:23 +0800

    修改路由

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -5,10 +5,16 @@ import './index.scss'
 
 class Navbar extends Component {
 	render() {
+		const configNav = [
+		  { text: '首页', url: '/', icon: 'iconshouye'},
+		  { text: '分类', url: '/category', icon: 'iconleimupinleifenleileibie'},
+		  { text: '购物车', url: '/car', icon: 'icongouwuche2'},
+		  { text: '我的', url: '/mine', icon: 'iconwode'}
+		]
 		return (
 			<ul className="tabBar">
 		          	{
-		                this.props.configNav.map((item,i) =>
+		                configNav.map((item,i) =>
 		                    <li key={i}>
 		                    	<NavLink exact to={item.url} activeClassName="active">
 			                    	<i className={`icon iconfont ${item.icon}`}></i>
--- src/Router/router.js
+++ src/Router/router.js
@@ -1,26 +1,27 @@
 import React from 'react'
-import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-import Home from '../pages/Home'
+import HomeView from '../pages/HomeView'      // 首页视图
 import ProductDetail from '../pages/ProductDetail'
 
 import Category from '../pages/Category'
 import Car from '../pages/Car'
 import Mine from '../pages/Mine'
 
+import Navbar from '../Components/Navbar'
+
 export default class IRouter extends React.Component{
     render() {
         return (
-            <HashRouter>
-                <Switch>
-                    <Route path="/" exact={true} component = { Home } />
-                    <Route path='/product/:id' component = { ProductDetail } />
-                    <Route path='/category' component = { Category } />
-                    <Route path='/car' component = { Car } />
-                    <Route path='/mine' component = { Mine } />
-                    <Redirect to="/" />
-                </Switch>
-            </HashRouter>
+            <BrowserRouter>                                    
+                <Route path="/" exact component = {HomeView} />
+                <Route path='/product/:id' component = { ProductDetail } />
+                <Route path='/category' component = { Category } />
+                <Route path='/car' component = { Car } />
+                <Route path='/mine' component = { Mine } />
+
+                <Route path={['/', '/category', '/car', '/mine']} exact component={Navbar}/>
+            </BrowserRouter>
         )
     }
 }　　
